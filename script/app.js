7a266b2359fdf69a2aef88d3aa0fac7452edf17a
David Jones <djones@breuer.com>
2012-10-15 15:29:57 -0400

    Up to First Card

---
+++ script/app.js
@@ -0,0 +1,37 @@
+
+$(function() { 
+
+	$("#demo-details").click(function(e){
+	
+		$(e.target).closest("div").toggleClass("selected");
+		
+		$(".cards").find(".card").fadeTo(0, 0);
+		$(".cards").slideToggle(200, function() {
+			$(this).find(".card").fadeTo(200, 1);
+		});
+	});
+
+	$("#profile-search").keyup(function(e) {
+		var text = $(this).val();
+
+		if(text == "") {
+			$(".card-category").show();
+		} else {
+			$(".card-category").hide();
+			$(".card-category").each(function(){
+				if($(this).text().toLowerCase().indexOf(text) != -1) {
+					$(this).show();
+				}
+			});
+		}
+	});
+
+	$(".card-actions .edit, .card-actions .save").click(function(e) {
+		var card = $(e.target).closest(".card"); 
+		card.find(".edit-toggle").toggle(); 
+		card.find(".card-actions .save:visible").css("display", "inline-block");
+		return false; 
+	})
+
+
+});
